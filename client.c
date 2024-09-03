378d5dccecc19ce4d2b2841f8d40d5d9aec0c025
Bushra Tehrani <137612478+tehr4ni@users.noreply.github.com>
2024-09-03 19:13:59 +1000

    Update client.c
    add send_signals function

--- client.c
+++ client.c
@@ -1,1 +1,24 @@
 
+void	send_signals(int pid, char *message)
+{
+	int	i; //to iterate through the char
+	int	bit_index; //to iterate over each bit index
+
+	bit_index = 7;
+	while (message[i] != '\0')
+	{
+		i = 0;
+		while (bit_index >= 0)
+		{
+      //shift what's in index 7 (MSB) to be in index 0 (LSB), then check if it's & 1. if it is 1, the condition is true, and the if condition executes
+			if ((c >> bit_index) & 1)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(50);
+			bit_index--; //decrement bit_index by 1 to analyse the next bit. 7->6, 6->5, and so on
+		}
+		i++;
+	}
+	return (0);
+}
