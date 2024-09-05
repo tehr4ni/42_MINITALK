c6281a222d911a3f7730155d0e9c424b1c5250ed
Bushra Tehrani <137612478+tehr4ni@users.noreply.github.com>
2024-09-05 18:46:49 +1000

    Update server.c
    Bettering the code and adding main function

--- server.c
+++ server.c
@@ -1,31 +1,36 @@
-// display PID
-void	display_pid(void) //? not sure if the parametre is supposed to be void
-{
-	int	pid;
-
-	pid = getpid();
-	ft_printf(pid); // fix this, not sure howw to display the pid
-}
-
-//create an endless loop so the server is constantly searching for signals
-while (1)
-{
-	pause(); //suspend the process until a signal is recieved
-}
-
-//recieve signals and handle each case accordingly (depending on whether the bit was 0 or 1)
+//recieve signals and handle the case for each signal/decrypt signal
 void	signal_handler(int signum)
 {
+	static unsigned char	character;
+	static int		bit_count; //for counting bits
+	static int		*result; //to hold the result message and print it as an str instead of printing chars one by one
+
+	char = 0;
+	bit_count = 0;
+	if (!result)
+		result = ft_strdup(""); //create an empty str to add chars to
 	if (signum == SIGUSR1)
-	{
-		// handle the case if the bit were 0
-	}
+		character = (character << 1); // shift left and add 0
 	else if (signum == SIGUSR2)
+		character = (character << 1) | 1; // shift left and add 1
+	bit_count++; 
+	if (bit_count == 8) //once you've recieved enough bits, make sense of the char
 	{
-		// handle the acse if the bit were 1
+		// append char to result
+		if (character == '\0')
+		{
+			ft_printf("%s\n", result);
+		}
+		character = 0;
+		bit_count = 0;
 	}
 }
 
-//decrypt signals: use SIGUSR1 to indicate you've recieved 1 ? idk
-
-// for each signal recieved, take a certain action
+int	main(void)
+{
+	while (1) // create an endless loop so the server keeps checking for signals
+		pause();
+	ft_printf("The server's PID: %d\n", getpid()); // display the PID
+	// signal handler
+	// sigaction stuff idk what it means
+}
