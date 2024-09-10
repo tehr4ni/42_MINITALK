467506d6cf9165f396f0aadd2fde6350040ab93b
Bushra Tehrani <137612478+tehr4ni@users.noreply.github.com>
2024-09-10 14:57:09 +1000

    Update client.c

--- client.c
+++ client.c
@@ -1,23 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btehrani <btehrani@student.42adel.org.au>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/09 10:57:57 by btehrani          #+#    #+#             */
+/*   Updated: 2024/09/09 15:42:48 by btehrani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
 
 void	send_signals(int pid, char *message)
 {
-	int	i; //to iterate through the char
-	int	bit_index; //to iterate over each bit index
+	int		i;
+	int		bit_index;
+	unsigned char	c;
 
-	bit_index = 7; //we start at the last bit index because we're going to shift it to the left one by one
-	while (message[i] != '\0')
+	i = 0;
+	while (message[i])
 	{
-		i = 0;
+		c = message[i];
+		bit_index = 7;
 		while (bit_index >= 0)
 		{
-			if ((c >> bit_index) & 1) //shift what's in the current bit index to be in index 0 (LSB), then check if it's & 1. if it is 1, the condition is true, and the if condition executes
+			if ((c >> bit_index) & 1) //c >> bit_index shifts bits of char c by 'bit_index' positions. &1 isolates LSB and checks if it is 1
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
-			usleep(42);
-			bit_index--; //decrement bit_index by 1 to analyse the next bit. 7->6, 6->5, and so on
+			usleep(50);
+			bit_index--;
 		}
 		i++;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int	server_pid;
+	char	*message;
+
+// error handling because the program can only run if there are 3 args
+	if (argc == 3)
+	{
+		server_pid = ft_atoi(argv[1]); //acquire server PID
+		if (!server_pid) //error handling in case there is no PID
+		{
+			ft_printf("ERROR: Wrong PID");
+			return (0);
+		}
+		message = argv[2]; //acquire message
+		send_signals(server_pid, message);
+		send_signals(server_pid, "\n"); //signal the end of the str and mark it with a new line
+	}
+	else
+	{
+		ft_printf("ERROR: Too many or too few arguments.\n Enter arguments");
+		ft_printf("as follows: ./client <PID> <message>");
+		return (1);
 	}
 	return (0);
 }
