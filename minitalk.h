31c39f76515beeb71f28084df8b8b9380f36ccee
Bushra Tehrani <137612478+tehr4ni@users.noreply.github.com>
2024-09-17 21:21:52 +1000

    Add files via upload

---
+++ minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btehrani <btehrani@student.42adel.org.au>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 14:05:16 by btehrani          #+#    #+#             */
+/*   Updated: 2024/09/10 19:28:52 by btehrani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
+# include "libft/libft.h"
+# include "printf/ft_printf.h"
+
+# endif
